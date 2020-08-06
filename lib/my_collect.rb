567eade54c56060ca19c32ada02feb460a458ec5
Stephen Junge <steve.junge@gmail.com>
2020-08-06 23:46:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+names = ["Maude Carrol", "Shannon Degnan", "Karen Faith"]
+
+def my_collect(array)
+  i = 0
+  full_names = []
+  while i < array.length
+    full_names << yield(array[i])
+    i += 1
+  end
+  full_names
+end
+
+my_collect(names) {|i| i.split(" ").first}
 
 
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
