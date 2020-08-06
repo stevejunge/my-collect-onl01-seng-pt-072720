724857e66e4f3da3a816fd4bcb10a2e3bec64db3
Stephen Junge <steve.junge@gmail.com>
2020-08-06 23:42:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,13 @@
-list = ["Tim Jones", "Bob Costas", "Don Knotts"]
+items = ["desk", "table", "couch"]
 
 def my_collect(array)
   i = 0
-  name_collection = []
+  living_room = []
   while i < array.length
-    name_collection.push yield(array[i])
-  # you could also do it this way:
-  # name_collection << yield(array[i])
+    name_collection << yield(array[i])
     i += 1
   end
-  name_collection
+  living_room
 end
 
-my_collect(list) {|i| i.split(" ").first}
+my_collect(items) {|i| i.split(" ").upcase}
