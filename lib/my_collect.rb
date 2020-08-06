11eb696993f9fde9b9c805f9937ebf20afbc6864
Stephen Junge <steve.junge@gmail.com>
2020-08-06 23:37:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,13 @@
 def my_collect(array)
   i = 0
-  names_array = []
-  while array[i] > array.length
-    yield 
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
